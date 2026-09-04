bd2f705e414fadf287a790883e57f5cb7dcbb0b9
tizianimamani8-ops <tizianimamani8@gmail.com>
2026-09-04 20:37:14 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".faq-item").forEach((item) => {
-  item.addEventListener("click", () => {
-    const answer = item.nextElementSibling;
-    const icon = item.querySelector("span:last-child");
-    const isOpen = answer.classList.toggle("open");
-    icon.textContent = isOpen ? "−" : "+";
-  });
-});
